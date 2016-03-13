afd3e8b7943ddcabeca415ea0da31df16de576a9
briancsimonsen <briancsimonsen@gmail.com>
2016-03-13 03:51:58 -0500

    Both the command and the watch folder should be nearly complete with the code included. Things left are retrieving a return code from FFmpeg to know when it is ready for another file, continually running the Directory Lister block to check for more files, and initial testing.

--- AutoConvert/bin/driver.py
+++ AutoConvert/bin/driver.py
@@ -1,28 +1,33 @@
 # Import Statements
 import subprocess as sp
+import os
 
 # Declare Global Variables
 FFMPEG = "/bin/ffmpeg.exe"
+watchPath = "./IN"
+destinationPath = "./OUT"
+ffmpegReady = 1
 
 # TODO Write Watch Folder Initialization
 
 
 
 # TODO Write File Information Retriever
-
+# Directory Lister
+fileQueue = []
+for files in os.listdir(watchPath):
+    info = os.stat(files)
+    fileQueue.append(info)
 
 
 # FFMPEG Launcher
 
 # FFMPEG Variables
-watchPath = ""
-destinationPath = ""
-fileName = "example"
-fileExtension = "mp4"
-codec = "dnxhd"
-
-# Command
-command = [ FFMPEG,
-            '-i', watchPath + fileName + fileExtension,
-            '-c:v', codec,
-            destinationPath + fileName + ".mxf"]
+if fileQueue.amount() > 0 and ffmpegReady == 1:
+    fileName, fileExtension = os.path.splitext(info)
+    codec = "libx264"
+    command = [FFMPEG,
+               '-i', watchPath + fileName + fileExtension,
+               '-c:v', codec,
+               destinationPath + fileName + ".mp4"]
+    pipe = sp.Popen(command)
