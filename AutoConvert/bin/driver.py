4928be87e4ef56fc5c7d8e9c509e6287581e04b2
briancsimonsen <briancsimonsen@gmail.com>
2016-03-13 03:34:58 -0500

    Add revision of FFmpeg command interface and create first version of file information retriever which doubles as watch folder

--- AutoConvert/bin/driver.py
+++ AutoConvert/bin/driver.py
@@ -1,28 +1,32 @@
 # Import Statements
 import subprocess as sp
+import os
 
 # Declare Global Variables
 FFMPEG = "/bin/ffmpeg.exe"
+watchPath = "./IN"
+destinationPath = "./OUT"
+ffmpegReady = 1
 
 # TODO Write Watch Folder Initialization
 
 
-
 # TODO Write File Information Retriever
-
+# Directory Lister
+fileQueue = []
+for files in os.listdir('./IN'):
+    info = os.stat(files)
+    fileQueue.append(info)
 
 
 # FFMPEG Launcher
 
 # FFMPEG Variables
-watchPath = ""
-destinationPath = ""
-fileName = "example"
-fileExtension = "mp4"
-codec = "dnxhd"
-
-# Command
-command = [ FFMPEG,
-            '-i', watchPath + fileName + fileExtension,
-            '-c:v', codec,
-            destinationPath + fileName + ".mxf"]
+if fileQueue.amount() > 0 and ffmpegReady == 1:
+    fileName, fileExtension = os.path.splitext(fileQueue.pop(0))
+    codec = "libx264"
+    command = [FFMPEG,
+               'ffmpeg',
+               '-c:v', codec,
+               destinationPath + fileName + ".mp4"]
+    pipe = sp.Popen(command)
