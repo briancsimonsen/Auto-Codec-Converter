562adba37a99da46cdc4f0ea5d90bd81fed9a336
wpreachuk <will.preachuk@gmail.com>
2016-03-15 19:34:03 -0500

    Fully Finished Part One, Now takes in and deletes files after conversion.

--- AutoConvert/bin/driver.py
+++ AutoConvert/bin/driver.py
@@ -1,30 +1,30 @@
 # Import Statements
 import subprocess as sp
 import os
+from time import sleep
 
 # Declare Global Variables
 FFMPEG = "./ffmpeg.exe"
 watchPath = "../IN"
 destinationPath = "../OUT"
 ffmpegReady = 1
-
-# TODO Write Watch Folder Initialization
-
-
-
-# TODO Write File Information Retriever
-# Directory Lister
 fileQueue = []
-for files in os.listdir(watchPath):
-    fileQueue.append(files)
 
-
-# FFMPEG Launcher
+def fileAdd():
+    for files in os.listdir(watchPath):
+        if(not(files in fileQueue)):
+            fileQueue.append(files)
 
 # FFMPEG Variables
-if len(fileQueue) > 0 and ffmpegReady == 1:
-    for f in fileQueue:
-        (fileName,fileExtension) = os.path.splitext(fileQueue.pop())
-        codec = "libx264"
-        command = [ FFMPEG, "-i", watchPath + '/' +fileName + fileExtension, "-c:v", codec, destinationPath + '/' + fileName + ".mp4", ]
-        sp.run(command)
+def run():
+    while True:
+        fileAdd()
+        if len(fileQueue) > 0 and ffmpegReady == 1:
+            for f in fileQueue:
+                (fileName,fileExtension) = os.path.splitext(fileQueue.pop())
+                codec = "libx264"
+                command = [ FFMPEG, "-i", watchPath + '/' +fileName + fileExtension, "-c:v", codec, destinationPath + '/' + fileName + ".mp4", ]
+                sp.check_output(command)
+                os.remove(watchPath + '/' + fileName + fileExtension)
+                sleep(5)
+run()
