f731ca11ffc4c21b7f48475134cf6064820d80f6
briancsimonsen <briancsimonsen@gmail.com>
2016-04-14 00:32:39 -0500

    Get Settings extended to add needed reference data from codec_library.xml comments added command finalized copyright no longer a feature

--- AutoConvert/bin/driver.py
+++ AutoConvert/bin/driver.py
@@ -14,14 +14,17 @@ fileQueue = []
 
 # Start Up Protocol
 def start_up_protocol():
-    #user_interface.user_interface()
+    # user_interface.user_interface()
     get_settings()
     validate_file_path()
     run()
 
 
+# Get settings from user_prefs.xml and needed reference data in codec_library.xml
 def get_settings():
+    # Attach user_prefs.xml
     root = eT.parse('user_prefs.xml').getroot()
+    # Declare and set User Preferences
     global watch_folder
     global destination_folder
     global video_codec
@@ -30,8 +33,8 @@ def get_settings():
     global audio_param
     global force_aspect
     global aspect
-    global force_copyright
-    global copyright
+    global v_codec_param
+    global new_file_extension
     watch_folder = root[0][0].text
     destination_folder = root[0][1].text
     video_codec = root[0][2].text
@@ -40,8 +43,16 @@ def get_settings():
     audio_param = root[0][5].text
     force_aspect = root[0][6].text
     aspect = root[0][7].text
-    force_copyright = root[0][8].text
-    copyright = root[0][9].text
+    # Attach codec_library.xml
+    library = eT.parse('codec_library.xml').getroot()
+    # iterate through encoders to look for correct codec to get encoder characteristics
+    for node in library.iter('encoder'):
+        for codec in node.iter('codec'):
+            if codec.text == video_codec:
+                encoder = node
+                break
+    v_codec_param = encoder[1].text
+    new_file_extension = encoder[6].text
 
 
 # Validate File Path
@@ -52,10 +63,11 @@ def validate_file_path():
         os.makedirs(destination_folder)
 
 
+# Add Files to File Queue
 def file_add():
     for files in os.listdir(watch_folder):
         amt1 = os.stat(watch_folder+"/"+files).st_size
-        sleep(.75)
+        sleep(3)
         amt2 = os.stat(watch_folder+"/"+files).st_size 
         if (not(files in fileQueue) and amt1==amt2):
             fileQueue.append(files)
@@ -67,12 +79,15 @@ def run():
         file_add()
         if len(fileQueue) > 0 and ffmpegReady == 1:
             for f in fileQueue:
-                (file_name, file_extension) = os.path.splitext(fileQueue.pop())
+                (file_name, old_file_extension) = os.path.splitext(fileQueue.pop())
                 command = [FFMPEG,
-                           "-i", watch_folder + '/' + file_name + file_extension,
-                           video_codec, "-crf", video_param,
-                           destination_folder + '/' + file_name + ".mp4",
+                           " -i ", watch_folder + '/' + file_name + old_file_extension, ' ',
+                           v_codec_param, ' ', video_param, ' ',
+                           destination_folder + '/' + file_name + new_file_extension
                            ]
-                sp.check_output(command)
-                os.remove(watch_folder + '/' + file_name + file_extension)
+                if force_aspect == True:
+                    command.append(" -aspect ")
+                    command.append(aspect)
+                sp.check_output(''.join(command))
+                os.remove(watch_folder + '/' + file_name + old_file_extension)
                 sleep(5)
