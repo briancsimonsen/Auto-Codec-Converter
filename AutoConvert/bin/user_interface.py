da04c5bde269e32cfca8088f311344bd0ea8ffd2
wpreachuk <will.preachuk@gmail.com>
2016-04-17 16:50:56 -0500

    Rough outline of TKINTER Set up, grid established and items set up.

--- AutoConvert/bin/user_interface.py
+++ AutoConvert/bin/user_interface.py
@@ -4,44 +4,45 @@ import driver
 # FFmpeg variables
 
 
-
 # TODO User Interface
 def user_interface():
     get_past_settings()
     global win
     win = Tk()
-    win.geometry("450x300")
+    win.geometry("500x350")
     win.title(" CCCP - Auto Codec Converter")
-    msg = Label(win, width = 25, text = "Enter watch folder path here")
-    msg.place(x=50,y=25)
+    msg = Label(win, width = 25, text = "Watch folder")
+    msg.grid(row=1,column=1,pady=5)
     eqn = Entry(win, width=25)
-    eqn.place(x=65,y=50)
-    msg = Label(win, width = 25, text = "Enter destination folder path here")
-    msg.place(x=50,y=75)
+    eqn.grid(row=2,column=1,pady=5)
+    msg = Label(win, width = 25, text = "Destination folder")
+    msg.grid(row=3,column=1,pady=5)
     eqn = Entry(win, width=25)
-    eqn.place(x=65,y=100)
+    eqn.grid(row=4,column=1,pady=5)
+    bitext = Label(win, width = 25, text = "Bit Rate").grid(row=5,column=1,pady=2)
+    bitslide = Scale(win, from_=0, to=5, orient=HORIZONTAL).grid(row=6,column=1,pady=5)
     msg2 = Label(win, width = 25, text = "Choose codec to convert to")
-    msg2.place(x=55,y=125)
-    ops = Listbox(win,height = 3)
-    ops.place(x=80,y=150)
-    ops.insert(1, "GoPro Cineform")
-    ops.insert(2, "AVI")
-    ops.insert(3, "DNxRAW")
+    msg2.grid(row=7,column=1,pady=5)
+    vary = StringVar(win)
+    vary.set("AVI")
+    ops = OptionMenu(win,vary,"GoPro Cineform","AVI","DNxRAW")
+    ops.configure(width=25)
+    ops.grid(row=8,column=1,pady=5)
+    #TODO HIGH QUAL SETTINGS
     active = Button(win,text = "Begin Conversion",command = start_convert)
-    active.place(x=210,y=230)
-    bitext = Label(win, width = 25, text = "Enter Bitrate Here").place(x=240,y=25)
-
-    bit = Entry(win,width = 25)
-    bit.insert(0,"30")
-    bit.place(x = 255, y=50)
+    active.grid(row=10,column=2,pady=5)
+    
 
     msg3 = Label(win, width = 25, text = "Choose Aspect Ratio")
-    msg3.place(x=235,y=125)
-    opsr = Listbox(win,height = 3)
-    opsr.place(x=270,y=150)
-    opsr.insert(1, "Auto")
-    opsr.insert(2, "4:3")
-    opsr.insert(3, "16:9")
+    msg3.grid(row=1,column=3,pady=5)
+    #TODO Fix the Aspect Ratio area
+    #opsr = Listbox(win,height = 5)
+    #opsr.grid(row=5,column=3,pady=5)
+    #opsr.insert(1, "Auto")
+    #opsr.insert(2, "4:3")
+    #opsr.insert(3, "16:9")
+    #opsr.insert(4, "1.3333")
+    #opsr.insert(5, "1.7777")
     win.mainloop()
 
 def start_convert():
@@ -59,8 +60,8 @@ def get_past_settings():
     pastSettings["destination_folder"] = root[0][1].text
     pastSettings["video_codec"] = root[0][2].text
     pastSettings["video_param"] = root[0][3].text
-    pastSettings["audio_codec"] = root[0][4].text
-    pastSettings["audio_param"] = root[0][5].text
+    #pastSettings["audio_codec"] = root[0][4].text #
+    #pastSettings["audio_param"] = root[0][5].text
     pastSettings["force_aspect"] = root[0][6].text
     pastSettings["aspect"] = root[0][7].text
 
@@ -70,3 +71,4 @@ def get_past_settings():
 
 # TODO Post Settings
 
+user_interface()
