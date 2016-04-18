6cfd0d99e45b1433e041b90669226be9144a533a
wpreachuk <will.preachuk@gmail.com>
2016-04-18 02:08:57 -0500

    Restyled the window assigned funtions to all window actions allowing them to set variables prepared the interface to validate and set the xml data

--- AutoConvert/bin/user_interface.py
+++ AutoConvert/bin/user_interface.py
@@ -8,25 +8,30 @@ import os
 # TODO User Interface
 def user_interface():
     get_past_settings()
-    #Creates a dictionary to store the new information. using od information
+
+    #Creates a dictionary to store the new information using old information
     currSettings = {}
     currSettings["watch_folder"] = pastSettings["watch_folder"]
     currSettings["destination_folder"] = pastSettings["destination_folder"]
     currSettings["video_codec"] = pastSettings["video_codec"]
-    currSettings["video_param"] = pastSettings["video_param"]
+    currSettings["video_param_mod"] = 1
     currSettings["force_aspect"] = pastSettings["force_aspect"]
     currSettings["aspect"] = pastSettings["aspect"]
+
     #establishes TK Window
     global win
     win = Tk()
-    win.geometry("500x350")
+    win.geometry("475x360")
     win.title(" CCCP - Auto Codec Converter")
+    win.resizable(width=FALSE, height=FALSE)
+
     #input filepath
     msg = Label(win, width = 25, text = "Watch folder")
-    msg.grid(row=1,column=1,pady=5)
-    inP = Entry(win, width=25)
+    msg.grid(row=1,column=1,pady=5,columnspan=2)
+    inP = Entry(win, width=29)
     inP.insert(0,currSettings["watch_folder"])
-    inP.grid(row=2,column=1,pady=5)
+    inP.grid(row=2,column=1,pady=5,columnspan=2)
+
     #Button for the input filepath
     def inBrowse():
         currSettings["watch_folder"] = filedialog.askdirectory()
@@ -34,12 +39,14 @@ def user_interface():
         inP.insert(0,currSettings["watch_folder"])
     inButton = Button(win,text="Browse",command = inBrowse)
     inButton.grid(row=2,column=2,pady=5)
+
     #Destination folder
     msg = Label(win, width = 25, text = "Destination folder")
-    msg.grid(row=3,column=1,pady=5)
-    outP = Entry(win, width=25)
+    msg.grid(row=3,column=1,pady=5,columnspan=2)
+    outP = Entry(win, width=29)
     outP.insert(0,currSettings["destination_folder"])
-    outP.grid(row=4,column=1,pady=5)
+    outP.grid(row=4,column=1,pady=5,columnspan=2)
+
     #Button for the output filepath
     def outBrowse():
         currSettings["destination_folder"] = filedialog.askdirectory()
@@ -47,39 +54,72 @@ def user_interface():
         outP.insert(0,currSettings["destination_folder"])
     outButton = Button(win,text="Browse",command = outBrowse)
     outButton.grid(row=4,column=2,pady=5)
+
     #Bitrate and Slider    
-    bitext = Label(win, width = 25, text = "Bit Rate").grid(row=5,column=1,pady=2)
-    bitslide = Scale(win, from_=0, to=5, orient=HORIZONTAL).grid(row=6,column=1,pady=5)
+    bitext = Label(win, width = 25, text = "                               Bit Rate")
+    bitext.grid(row=5,column=1,pady=2)
+    bitvar = DoubleVar()
+    def bitrModSet(value):
+        currSettings["video_param_mod"] = (1/5.0) * bitvar.get() 
+    bitslide = Scale(win,variable=bitvar, from_=1, to=5, orient=HORIZONTAL,length=175,command=bitrModSet)
+    bitslide.set(6)
+    bitslide.grid(row=6,column=1,pady=5,columnspan=2)
+
     #Codec Dropdown
     msg2 = Label(win, width = 25, text = "Output Codec")
-    msg2.grid(row=7,column=1,pady=5)
+    msg2.grid(row=7,column=1,pady=5,columnspan=2)
     vary = StringVar(win)
     vary.set(currSettings["video_codec"])
-    
-    ops = OptionMenu(win,vary,"GoPro Cineform","AVI","DNxRAW","MPEG-4")
-    ops.configure(width=20)
-    ops.grid(row=8,column=1,pady=5)
-    #TODO HIGH QUAL SETTINGS
-    #Activation Button
-    active = Button(win,text = "Begin Conversion",command = start_convert)
-    active.grid(row=10,column=2,pady=5)
+    def cSet(x):
+        currSettings["video_codec"] = x
+    ops = OptionMenu(win,vary,"GoPro Cineform","AVI","DNxRAW","MPEG-4", command=cSet)
+    ops.configure(width=25)
+    ops.grid(row=8,column=1,pady=5,columnspan=2)
+
+    #High Quality Settings
+    vary2 = IntVar(win)
+    vary2.set(currSettings["force_aspect"])
+    def hqSet():
+        if (currSettings["force_aspect"]==True):
+            currSettings["force_aspect"] = False
+        else:
+            currSettings["force_aspect"] = True 
+    hqs = Checkbutton(win,text="Force High Quality Settings",variable=vary2,onvalue=1,offvalue=0,command=hqSet)
+    hqs.grid(row=9,column=1,pady=5,columnspan=2)
     
     #Aspect Ratio
     msg3 = Label(win, width = 25, text = "Choose Aspect Ratio")
     msg3.grid(row=1,column=3,pady=5)
-    opsr = Listbox(win,height = 5)
-    opsr.grid(row=2,column=3,pady=5,rowspan=4)
-    opsr.insert(1, "Auto")
-    opsr.insert(2, "4:3")
-    opsr.insert(3, "16:9")
-    opsr.insert(4, "1.3333")
-    opsr.insert(5, "1.7777")
-    win.mainloop()
+    def radSet():
+        currSettings["aspect"] = vary3.get()
 
-def start_convert():
-    #xml_write()
-    win.destroy()
-    driver.start_up_protocol()
+    vary3 = StringVar()
+    vary3.set(currSettings["aspect"])
+    opsr1 = Radiobutton(win,variable=vary3,text="Auto",value="Auto",command=radSet)
+    opsr1.grid(row=2,column=3,pady=3)
+    opsr2 = Radiobutton(win,variable=vary3,text="4:3",value="4:3",command=radSet)
+    opsr2.grid(row=3,column=3,pady=3)
+    opsr3 = Radiobutton(win,variable=vary3,text="16:9",value="16:9",command=radSet)
+    opsr3.grid(row=4,column=3,pady=3)
+    opsr4 = Radiobutton(win,variable=vary3,text="1.3333",value="1.3333",command=radSet)
+    opsr4.grid(row=5,column=3,pady=3)
+    opsr5 = Radiobutton(win,variable=vary3,text="1.7777",value="1.7777",command=radSet)
+    opsr5.grid(row=6,column=3,pady=3)
+    
+    #Activation Button
+    
+    def start_convert():
+        #if(not(isValid())):
+            #print terrible things
+        #else:
+        #xml_write()
+        win.destroy()
+        driver.start_up_protocol()
+    active = Button(win,text = "Begin Conversion",command = start_convert)
+    active.grid(row=10,column=2,pady=5)
+    
+    #Mainloop
+    win.mainloop()
     
 # TODO Get Past Settings
 def get_past_settings():
@@ -91,8 +131,6 @@ def get_past_settings():
     pastSettings["destination_folder"] = root[0][1].text
     pastSettings["video_codec"] = root[0][2].text
     pastSettings["video_param"] = root[0][3].text
-    #pastSettings["audio_codec"] = root[0][4].text #
-    #pastSettings["audio_param"] = root[0][5].text
     pastSettings["force_aspect"] = root[0][6].text
     pastSettings["aspect"] = root[0][7].text
 
@@ -101,5 +139,3 @@ def get_past_settings():
 
 
 # TODO Post Settings
-
-user_interface()
