30ece0575297f681d7a180afbb5ee5dbd54e2b7f
briancsimonsen <briancsimonsen@gmail.com>
2016-04-19 00:59:33 -0500

    Video Files Only in File Queue
    The file queue will now only add video files and also the code has been re-factored for proper spacing and alignment

--- AutoConvert/bin/driver.py
+++ AutoConvert/bin/driver.py
@@ -1,18 +1,22 @@
 # Import Statements
-import user_interface
-import subprocess as sp
 import os
+import subprocess as sp
 import xml.etree.ElementTree as eT
 from time import sleep
 
+import user_interface
 
 # Declare Global Variables
 FFMPEG = "./ffmpeg.exe"
 ffmpegReady = 1
 fileQueue = []
+global video_extentions
+
 
 def start_process():
     user_interface.user_interface()
+
+
 # Start Up Protocol
 def start_up_protocol():
     get_settings()
@@ -66,10 +70,11 @@ def validate_file_path():
 # Add Files to File Queue
 def file_add():
     for files in os.listdir(watch_folder):
-        amt1 = os.stat(watch_folder+"/"+files).st_size
+        amt1 = os.stat(watch_folder + "/" + files).st_size
         sleep(3)
-        amt2 = os.stat(watch_folder+"/"+files).st_size 
-        if (not(files in fileQueue) and amt1==amt2):
+        amt2 = os.stat(watch_folder + "/" + files).st_size
+        (file_name, old_file_extension) = os.path.splitext(files)
+        if (not (files in fileQueue) and amt1 == amt2 and old_file_extension in video_extentions):
             fileQueue.append(files)
 
 
@@ -82,7 +87,7 @@ def run():
                 (file_name, old_file_extension) = os.path.splitext(fileQueue.pop())
                 command = [FFMPEG,
                            " -i ", "\"" + watch_folder + '/' + file_name + old_file_extension + "\"", ' ',
-                           v_codec_param, ' ', video_param, ' ', 
+                           v_codec_param, ' ', video_param, ' ',
                            "\"" + destination_folder + '/' + file_name + new_file_extension + "\""
                            ]
                 commandString = ""
@@ -94,3 +99,39 @@ def run():
                 sp.check_output(commandString)
                 os.remove(watch_folder + '/' + file_name + old_file_extension)
                 sleep(5)
+
+
+video_extentions = ['.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf',
+                    '.aec', '.aep', '.aepx', '.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim',
+                    '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb', '.avc', '.avd', '.avi', '.avp', '.avs', '.avs',
+                    '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik', '.bin', '.bix', '.bmk', '.bnp', '.box',
+                    '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine', '.cip',
+                    '.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v',
+                    '.dat', '.dav', '.dce', '.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb',
+                    '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss', '.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy',
+                    '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr', '.dzm', '.dzp', '.dzt',
+                    '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject',
+                    '.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi',
+                    '.gvp', '.h264', '.hdmov', '.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism',
+                    '.ismc', '.ismv', '.iva', '.ivf', '.ivr', '.ivs', '.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g',
+                    '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg', '.m1v', '.m21', '.m21', '.m2a', '.m2p',
+                    '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv', '.mj2', '.mjp',
+                    '.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov',
+                    '.movie', '.mp21', '.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf',
+                    '.mpg', '.mpg2', '.mpgindex', '.mpl', '.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd',
+                    '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc', '.mvd', '.mve', '.mvex', '.mvp', '.mvp',
+                    '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv', '.nvc', '.ogm', '.ogv', '.ogx',
+                    '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist',
+                    '.plproj', '.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh',
+                    '.pssd', '.pva', '.pvr', '.pxv', '.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd',
+                    '.rcproject', '.rdb', '.rec', '.rm', '.rmd', '.rmd', '.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp',
+                    '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt', '.scc', '.scm', '.scm',
+                    '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi',
+                    '.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx',
+                    '.svi', '.swf', '.swi', '.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0',
+                    '.tpd', '.tpr', '.trp', '.ts', '.tsp', '.ttxt', '.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr',
+                    '.vcv', '.vdo', '.vdr', '.vdx', '.veg', '.vem', '.vep', '.vf', '.vft', '.vfw', '.vfz', '.vgz',
+                    '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7', '.vpj',
+                    '.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv',
+                    '.wmx', '.wot', '.wp3', '.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv',
+                    '.xmv', '.xvid', '.y4m', '.yog', '.yuv', '.zeg', '.zm1', '.zm2', '.zm3', '.zmv']
--- AutoConvert/bin/user_interface.py
+++ AutoConvert/bin/user_interface.py
@@ -1,8 +1,10 @@
 import xml.etree.ElementTree as eT
-from tkinter import *
-import driver
-import os
 from math import ceil
+from tkinter import *
+
+import driver
+
+
 # FFmpeg variables
 
 
@@ -10,7 +12,7 @@ from math import ceil
 def user_interface():
     get_past_settings()
 
-    #Creates a dictionary to store the new information using old information
+    # Creates a dictionary to store the new information using old information
     currSettings = {}
     currSettings["watch_folder"] = pastSettings["watch_folder"]
     currSettings["destination_folder"] = pastSettings["destination_folder"]
@@ -19,112 +21,124 @@ def user_interface():
     currSettings["force_aspect"] = pastSettings["force_aspect"]
     currSettings["aspect"] = pastSettings["aspect"]
 
-    #establishes TK Window
+    # establishes TK Window
     global win
     win = Tk()
     win.geometry("475x360")
     win.title(" CCCP - Auto Codec Converter")
     win.resizable(width=FALSE, height=FALSE)
 
-    #input filepath
-    msg = Label(win, width = 25, text = "Watch folder")
-    msg.grid(row=1,column=1,pady=5,columnspan=2)
+    # input filepath
+    msg = Label(win, width=25, text="Watch folder")
+    msg.grid(row=1, column=1, pady=5, columnspan=2)
     inP = Entry(win, width=29)
-    inP.insert(0,currSettings["watch_folder"])
-    inP.grid(row=2,column=1,pady=5,columnspan=2)
+    inP.insert(0, currSettings["watch_folder"])
+    inP.grid(row=2, column=1, pady=5, columnspan=2)
 
-    #Button for the input filepath
+    # Button for the input filepath
     def inBrowse():
         currSettings["watch_folder"] = filedialog.askdirectory()
-        inP.delete(0,END)
-        inP.insert(0,currSettings["watch_folder"])
-    inButton = Button(win,text="Browse",command = inBrowse)
-    inButton.grid(row=2,column=2,pady=5)
+        inP.delete(0, END)
+        inP.insert(0, currSettings["watch_folder"])
 
-    #Destination folder
-    msg = Label(win, width = 25, text = "Destination folder")
-    msg.grid(row=3,column=1,pady=5,columnspan=2)
+    inButton = Button(win, text="Browse", command=inBrowse)
+    inButton.grid(row=2, column=2, pady=5)
+
+    # Destination folder
+    msg = Label(win, width=25, text="Destination folder")
+    msg.grid(row=3, column=1, pady=5, columnspan=2)
     outP = Entry(win, width=29)
-    outP.insert(0,currSettings["destination_folder"])
-    outP.grid(row=4,column=1,pady=5,columnspan=2)
+    outP.insert(0, currSettings["destination_folder"])
+    outP.grid(row=4, column=1, pady=5, columnspan=2)
 
-    #Button for the output filepath
+    # Button for the output filepath
     def outBrowse():
         currSettings["destination_folder"] = filedialog.askdirectory()
-        outP.delete(0,END)
-        outP.insert(0,currSettings["destination_folder"])
-    outButton = Button(win,text="Browse",command = outBrowse)
-    outButton.grid(row=4,column=2,pady=5)
+        outP.delete(0, END)
+        outP.insert(0, currSettings["destination_folder"])
 
-    #Bitrate and Slider    
-    bitext = Label(win, width = 25, text = "                               Bit Rate")
-    bitext.grid(row=5,column=1,pady=2)
+    outButton = Button(win, text="Browse", command=outBrowse)
+    outButton.grid(row=4, column=2, pady=5)
+
+    # Bitrate and Slider
+    bitext = Label(win, width=25, text="                               Bit Rate")
+    bitext.grid(row=5, column=1, pady=2)
     bitvar = DoubleVar()
-    def bitrModSet(value):
-        currSettings["video_param_mod"] = (1/5.0) * bitvar.get() 
-    bitslide = Scale(win,variable=bitvar, from_=0, to=5, orient=HORIZONTAL,length=175,command=bitrModSet)
-    bitslide.set(6)
-    bitslide.grid(row=6,column=1,pady=5,columnspan=2)
 
-    #Codec Dropdown
-    msg2 = Label(win, width = 25, text = "Output Codec")
-    msg2.grid(row=7,column=1,pady=5,columnspan=2)
+    def bitrModSet(value):
+        currSettings["video_param_mod"] = (1 / 5.0) * bitvar.get()
+
+    bitslide = Scale(win, variable=bitvar, from_=0, to=5, orient=HORIZONTAL, length=175, command=bitrModSet)
+    bitslide.set(6)
+    bitslide.grid(row=6, column=1, pady=5, columnspan=2)
+
+    # Codec Dropdown
+    msg2 = Label(win, width=25, text="Output Codec")
+    msg2.grid(row=7, column=1, pady=5, columnspan=2)
     vary = StringVar(win)
     vary.set(currSettings["video_codec"])
+
     def cSet(x):
         currSettings["video_codec"] = x
-    codecz = ["H.264","MPEG-4","H.265","HEVC","Xvid MPEG-4","VP8","VP9","Apple ProRes","MPEG-1","MPEG-2","MPEG-4 Option 2","FLV","Theora"]
-    list.sort(codecz)
-    ops = OptionMenu(win,vary,*codecz, command=cSet)
-    ops.configure(width=25)
-    ops.grid(row=8,column=1,pady=5,columnspan=2)
 
-    #High Quality Settings
+    codecz = ["H.264", "MPEG-4", "H.265", "HEVC", "Xvid MPEG-4", "VP8", "VP9", "Apple ProRes", "MPEG-1", "MPEG-2",
+              "MPEG-4 Option 2", "FLV", "Theora"]
+    list.sort(codecz)
+    ops = OptionMenu(win, vary, *codecz, command=cSet)
+    ops.configure(width=25)
+    ops.grid(row=8, column=1, pady=5, columnspan=2)
+
+    # High Quality Settings
     vary2 = IntVar(win)
     vary2.set(currSettings["force_aspect"])
+
     def hqSet():
-        if (currSettings["force_aspect"]==True):
+        if (currSettings["force_aspect"] == True):
             currSettings["force_aspect"] = False
         else:
-            currSettings["force_aspect"] = True 
-    hqs = Checkbutton(win,text="Force High Quality Settings",variable=vary2,onvalue=1,offvalue=0,command=hqSet)
-    hqs.grid(row=9,column=1,pady=5,columnspan=2)
-    
-    #Aspect Ratio
-    msg3 = Label(win, width = 25, text = "Choose Aspect Ratio")
-    msg3.grid(row=1,column=3,pady=5)
+            currSettings["force_aspect"] = True
+
+    hqs = Checkbutton(win, text="Force High Quality Settings", variable=vary2, onvalue=1, offvalue=0, command=hqSet)
+    hqs.grid(row=9, column=1, pady=5, columnspan=2)
+
+    # Aspect Ratio
+    msg3 = Label(win, width=25, text="Choose Aspect Ratio")
+    msg3.grid(row=1, column=3, pady=5)
+
     def radSet():
         currSettings["aspect"] = vary3.get()
 
     vary3 = StringVar()
     vary3.set(currSettings["aspect"])
-    opsr1 = Radiobutton(win,variable=vary3,text="Auto",value="Auto",command=radSet)
-    opsr1.grid(row=2,column=3,pady=3)
-    opsr2 = Radiobutton(win,variable=vary3,text="4:3",value="4:3",command=radSet)
-    opsr2.grid(row=3,column=3,pady=3)
-    opsr3 = Radiobutton(win,variable=vary3,text="16:9",value="16:9",command=radSet)
-    opsr3.grid(row=4,column=3,pady=3)
-    opsr4 = Radiobutton(win,variable=vary3,text="1.3333",value="1.3333",command=radSet)
-    opsr4.grid(row=5,column=3,pady=3)
-    opsr5 = Radiobutton(win,variable=vary3,text="1.7777",value="1.7777",command=radSet)
-    opsr5.grid(row=6,column=3,pady=3)
-    
-    #Activation Button
-    
-        
+    opsr1 = Radiobutton(win, variable=vary3, text="Auto", value="Auto", command=radSet)
+    opsr1.grid(row=2, column=3, pady=3)
+    opsr2 = Radiobutton(win, variable=vary3, text="4:3", value="4:3", command=radSet)
+    opsr2.grid(row=3, column=3, pady=3)
+    opsr3 = Radiobutton(win, variable=vary3, text="16:9", value="16:9", command=radSet)
+    opsr3.grid(row=4, column=3, pady=3)
+    opsr4 = Radiobutton(win, variable=vary3, text="1.3333", value="1.3333", command=radSet)
+    opsr4.grid(row=5, column=3, pady=3)
+    opsr5 = Radiobutton(win, variable=vary3, text="1.7777", value="1.7777", command=radSet)
+    opsr5.grid(row=6, column=3, pady=3)
+
+    # Activation Button
+
+
     def start_convert():
-        #if(not(isValid())):
-            #print terrible things
-        #else:
+        # if(not(isValid())):
+        # print terrible things
+        # else:
         postSettings(currSettings)
         win.destroy()
         driver.start_up_protocol()
-    active = Button(win,text = "Begin Conversion",command = start_convert)
-    active.grid(row=10,column=2,pady=5)
-    
-    #Mainloop
+
+    active = Button(win, text="Begin Conversion", command=start_convert)
+    active.grid(row=10, column=2, pady=5)
+
+    # Mainloop
     win.mainloop()
-    
+
+
 # TODO Get Past Settings
 def get_past_settings():
     root = eT.parse('user_prefs.xml').getroot()
@@ -147,12 +161,12 @@ def postSettings(dicto):
     tree = eT.parse('user_prefs.xml')
     root = tree.getroot()
     # Declare and set User Preferences
-    #global pastSettings
-    #pastSettings = {}
-    root[0][0].text = str(dicto["watch_folder"])  
-    root[0][1].text = str(dicto["destination_folder"])  
+    # global pastSettings
+    # pastSettings = {}
+    root[0][0].text = str(dicto["watch_folder"])
+    root[0][1].text = str(dicto["destination_folder"])
     root[0][2].text = str(dicto["video_codec"])
-    #Write the Codec param using the codec info
+    # Write the Codec param using the codec info
     library = eT.parse('codec_library.xml').getroot()
     # iterate through encoders to look for correct codec to get encoder characteristics
     pMin = 0
@@ -162,16 +176,15 @@ def postSettings(dicto):
             if codec.text == str(dicto["video_codec"]):
                 pMin = int(node[2].text)
                 pMax = int(node[3].text)
-    pInt = pMax-pMin
+    pInt = pMax - pMin
     pVal = pInt * float(dicto["video_param_mod"])
     pVal = int(ceil(pVal))
-    if(pInt <0):
+    if (pInt < 0):
         pVal *= -1
-        setter = pMin-pVal
+        setter = pMin - pVal
     else:
-        setter = pMin+pVal
-    root[0][3].text = str(setter) 
-    root[0][6].text = str(dicto["force_aspect"]) 
+        setter = pMin + pVal
+    root[0][3].text = str(setter)
+    root[0][6].text = str(dicto["force_aspect"])
     root[0][7].text = str(dicto["aspect"])
-    tree.write('user_prefs.xml')#user_prefs
-    
+    tree.write('user_prefs.xml')  # user_prefs
