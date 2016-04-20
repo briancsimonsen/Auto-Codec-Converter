812700ea8bb2c8675bd98af77fa996bea170e4ae
wpreachuk <will.preachuk@gmail.com>
2016-04-20 15:52:05 -0500

    List of codecs is now dynamically pulled from the codec library ERROR FIX For exiting out of folder selection window. Directory now starts form C:\\ relocation fo grid elements, to allow for the full path to be scrolled through

--- AutoConvert/bin/user_interface.py
+++ AutoConvert/bin/user_interface.py
@@ -37,12 +37,15 @@ def user_interface():
 
     # Button for the input filepath
     def inBrowse():
-        currSettings["watch_folder"] = filedialog.askdirectory()
+        currSettings["watch_folder"] = filedialog.askdirectory(initialdir="C:\\")
         inP.delete(0, END)
         inP.insert(0, currSettings["watch_folder"])
+        if(inP.get()==""):
+            inP.insert(0,pastSettings["watch_folder"])
+            currSettings["watch_folder"] = pastSettings["watch_folder"]
 
     inButton = Button(win, text="Browse", command=inBrowse)
-    inButton.grid(row=2, column=2, pady=5)
+    inButton.grid(row=2, column=3, pady=5)
 
     # Destination folder
     msg = Label(win, width=25, text="Destination folder")
@@ -53,23 +56,26 @@ def user_interface():
 
     # Button for the output filepath
     def outBrowse():
-        currSettings["destination_folder"] = filedialog.askdirectory()
+        currSettings["destination_folder"] = filedialog.askdirectory(initialdir="C:\\")
         outP.delete(0, END)
         outP.insert(0, currSettings["destination_folder"])
+        if(outP.get()==""):
+            outP.insert(0,pastSettings["destination_folder"])
+            currSettings["destination_folder"] = pastSettings["destination_folder"]
 
     outButton = Button(win, text="Browse", command=outBrowse)
-    outButton.grid(row=4, column=2, pady=5)
+    outButton.grid(row=4, column=3, pady=5)
 
     # Bitrate and Slider
-    bitext = Label(win, width=25, text="                               Bit Rate")
+    bitext = Label(win, width=25, text="        Bit Rate")
     bitext.grid(row=5, column=1, pady=2)
     bitvar = DoubleVar()
 
     def bitrModSet(value):
-        currSettings["video_param_mod"] = (1 / 5.0) * bitvar.get()
+        currSettings["video_param_mod"] = (1 / 50.0) * bitvar.get()
 
-    bitslide = Scale(win, variable=bitvar, from_=0, to=5, orient=HORIZONTAL, length=175, command=bitrModSet)
-    bitslide.set(6)
+    bitslide = Scale(win, variable=bitvar, from_=0, to=50, orient=HORIZONTAL, length=175, command=bitrModSet)
+    bitslide.set(50)
     bitslide.grid(row=6, column=1, pady=5, columnspan=2)
 
     # Codec Dropdown
@@ -81,8 +87,11 @@ def user_interface():
     def cSet(x):
         currSettings["video_codec"] = x
 
-    codecz = ["H.264", "MPEG-4", "H.265", "HEVC", "Xvid MPEG-4", "VP8", "VP9", "Apple ProRes", "MPEG-1", "MPEG-2",
-              "MPEG-4 Option 2", "FLV", "Theora"]
+    codecz = []
+    library = eT.parse('codec_library.xml').getroot()
+    for node in library[0].iter('encoder'):
+        for codec in node.iter('codec'):
+            codecz.append(codec.text)
     list.sort(codecz)
     ops = OptionMenu(win, vary, *codecz, command=cSet)
     ops.configure(width=25)
@@ -103,7 +112,7 @@ def user_interface():
 
     # Aspect Ratio
     msg3 = Label(win, width=25, text="Choose Aspect Ratio")
-    msg3.grid(row=1, column=3, pady=5)
+    msg3.grid(row=1, column=4, pady=5)
 
     def radSet():
         currSettings["aspect"] = vary3.get()
@@ -111,15 +120,15 @@ def user_interface():
     vary3 = StringVar()
     vary3.set(currSettings["aspect"])
     opsr1 = Radiobutton(win, variable=vary3, text="Auto", value="Auto", command=radSet)
-    opsr1.grid(row=2, column=3, pady=3)
+    opsr1.grid(row=2, column=4, pady=3)
     opsr2 = Radiobutton(win, variable=vary3, text="4:3", value="4:3", command=radSet)
-    opsr2.grid(row=3, column=3, pady=3)
+    opsr2.grid(row=3, column=4, pady=3)
     opsr3 = Radiobutton(win, variable=vary3, text="16:9", value="16:9", command=radSet)
-    opsr3.grid(row=4, column=3, pady=3)
+    opsr3.grid(row=4, column=4, pady=3)
     opsr4 = Radiobutton(win, variable=vary3, text="1.3333", value="1.3333", command=radSet)
-    opsr4.grid(row=5, column=3, pady=3)
+    opsr4.grid(row=5, column=4, pady=3)
     opsr5 = Radiobutton(win, variable=vary3, text="1.7777", value="1.7777", command=radSet)
-    opsr5.grid(row=6, column=3, pady=3)
+    opsr5.grid(row=6, column=4, pady=3)
 
     # Activation Button
 
@@ -133,7 +142,7 @@ def user_interface():
         driver.start_up_protocol()
 
     active = Button(win, text="Begin Conversion", command=start_convert)
-    active.grid(row=10, column=2, pady=5)
+    active.grid(row=10, column=2, pady=5,columnspan=2)
 
     # Mainloop
     win.mainloop()
